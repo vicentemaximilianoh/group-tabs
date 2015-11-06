55b7d9024a5a50857ec4861e7635849c345f4bbf
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2015-11-06 02:05:21 -0300

    Added gulp tasks for production deployment.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var del = require('del');
 var concat = require('gulp-concat');
 var usemin = require('gulp-usemin');
 var rename = require('gulp-rename');
+var replace = require('gulp-replace');
 var copy = require('gulp-copy');
 var rimraf = require('gulp-rimraf');
 var runSequence = require('run-sequence');
@@ -20,51 +21,76 @@ var paths = {
 };
 
 gulp.task('clean', function (done) {
-        del(paths);
-        done();
+    return del(['./dist'], done);
 });
 
-gulp.task('images', function (done) {
-        gulp.src('./app/img/*')
-        .pipe(copy('./dist/img', { prefix: 2}))
+gulp.task('minify', function (done) {
+    gulp.src('./dist/index.html')
+        .pipe(usemin({
+            assetsDir: './',
+            css: [minifyCss, 'concat'],
+            js: [uglify(), 'concat']
+        }))
+        .pipe(gulp.dest('./dist'))
         .on('end', done);
 });
 
-gulp.task('fix-template', function (done) {
-    gulp.src('./dist/window.src.html')
-    .pipe(rimraf())
-    .pipe(rename('window.html'))
-    .pipe(gulp.dest('app/'))
-    .on('end', done);
+gulp.task('images', function (done) {
+    gulp.src('./app/img/*')
+        .pipe(gulp.dest('./dist/img/'))
+        .on('end', done);
 });
 
 
-gulp.task('minify', function (done) {
-    gulp.src('./app/window.src.html')
-    .pipe(usemin({
-        assetsDir: './',
-        css: [minifyCss, 'concat'],
-        js: [uglify(), 'concat']
-    }))
-    .pipe(gulp.dest('dist'))
-    .on('end', done);
+gulp.task('copy-templates', function (done) {
+    gulp.src('./app/js/**/*.html')
+        .pipe(replace(/\/app\//g, '/dist/'))
+        .pipe(gulp.dest('./dist/js/'))
+        .on('end', done);
 });
 
+gulp.task('fix-templates', function (done) {
+    gulp.src('./dist/js/app.js')
+        .pipe(replace(/\/app\//g, '/'))
+        .pipe(gulp.dest('./dist/js/'))
+        .on('end', done);
+});
+
+gulp.task('fix-index', function (done) {
+    gulp.src('./app/index.html')
+        .pipe(copy('./dist/', {
+            prefix: 1
+        }))
+        .on('end', done);
+});
+
+/*
 gulp.task('watch', ['default'], function (done) {
     var watchFiles = [
-        './dist/**/*'
+        './dist/**'
     ];
-    gulp.watch('./app/**/*', ['minify'])
-    .on('end', done);
+    gulp.watch('./app/**')
+        .on('end', done);
+});
+*/
+
+gulp.task('fix-manifest', function (done) {
+    gulp.src(['manifest.json'])
+        .pipe(replace(/\/app\//g, './'))
+        .pipe(gulp.dest('./dist'))
+        .on('end', done);
+});
+
+gulp.task('copy-background', function (done) {
+    gulp.src(['background.js'])
+        .pipe(gulp.dest('./dist'))
+        .on('end', done);
 });
 
 gulp.task('default', function (done) {
-    
+
     runSequence(
-        'clean',
-        ['minify', 'images'],
-        'fix-template',
-        done
+        'clean', ['fix-index', 'fix-manifest', 'copy-background'], ['minify', 'images'], ['copy-templates', 'fix-templates'], done
     );
-    
+
 });
